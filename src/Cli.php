f4a8e3838c32625629b83391ed511427ce68be1a
Zharkov Denis <zdu.ing@gmail.com>
2019-03-12 22:02:38 +0600

    add games

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,21 @@
 <?php
 namespace BrainGames\Cli;
-
 use function \cli\line;
 use function \cli\prompt;
-
+use function BrainGames\games\GameBrainEven\run as gameEven;
+use function BrainGames\games\GameBrainCalc\run as gameCalc;
+use function BrainGames\games\GameBrainGCD\run as gameGCV;
 function run()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Welcome to the Brain Games!");
+    line("Menu:");
+    line("1 - Brain Even Game");
+    line("2 - Brain Calc Game");
+    line("any other - EXIT");
+    $answerUser = prompt("\nEnter number of game to run it: ");
+    if ($answerUser == 1) {
+        gameEven();
+    } elseif ($answerUser == 2) {
+        gameCalc();
+    }
 }
