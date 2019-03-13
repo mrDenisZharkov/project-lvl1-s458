d7fc06f347948ccd43f0c6e8c7d803283f6c367e
Zharkov Denis <zdu.ing@gmail.com>
2019-03-13 21:32:53 +0600

    add engine

---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+namespace BrainGames\Engine;
+
+use function \cli\line;
+use function \cli\prompt;
+const COUNT_ROUNDS = 3;
+
+function play(string $title, string $rule, callable $question)
+{
+    line("\tWelcome to the {$title}!");
+    line("\n{$rule}");
+    $username = prompt("\nMay I have your name?");
+    line("\tHello, {$username}!");
+    for ($i = 0; $i < COUNT_ROUNDS; $i++) {
+        line("\nQuestion: ");
+        $correctAnswer = $question();
+        $playerAnswer = prompt("\nYour answer: ");
+        $winIndicator = true;
+        if ($playerAnswer == $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("'{$playerAnswer}' is wrong answer. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, {$username}...");
+            return;
+	    }
+    }
+    line("\n\tCongratulations, {$username}!");
+}
+
--- src/Games/BrainEven.php
+++ src/Games/BrainEven.php
@@ -3,49 +3,24 @@ namespace BrainGames\Games\BrainEven;
 
 use function \cli\line;
 use function \cli\prompt;
+use function BrainGames\Engine\play;
 
-//Constants (settable)
-
-const RULE = "Answer 'yes' if number even otherwise answer 'no'.";
 const TITLE = "Brain Even Game";
+const RULE = "Answer 'yes' if number even otherwise answer 'no'.";
 
-//Question description (writeable)
 function isEven($number)
 {
 	return ($number % 2 === 0) ? true : false;
 }
-function question()
-{
-	$minGen = 1;
-	$maxGen = 100;
-    $questionNum = rand($minGen, $maxGen);
-    print_r($questionNum);
-    return (isEven($questionNum)) ? "yes" : "no";
-}
 function run()
 {
-	play(TITLE, RULE);
+    play(TITLE, RULE, function () {
+		$minGen = 1;
+	    $maxGen = 100;
+        $question = rand($minGen, $maxGen);
+        print_r($question);
+        return isEven($question) ? "yes" : "no";
+	});
 }
-function play($title, $rule)
-{
-	$countrounds = 3;
-    line("\tWelcome to the {$title}!");
-    line("\n{$rule}");
-    $username = prompt("\nMay I have your name?");
-    line("\tHello, {$username}!");
-    for ($i = 0; $i < $countrounds; $i++) {
-        line("\nQuestion: ");
-        $correctAnswer = question();
-        $playerAnswer = prompt("\nYour answer: ");
-        $winIndicator = true;
-        if ($playerAnswer == $correctAnswer) {
-            line("Correct!");
-        } else {
-            line("'{$playerAnswer}' is wrong answer. Correct answer was '{$correctAnswer}'.");
-            line("Let's try again, {$username}...");
-            return;
-	    }
-    }
-    line("\n\tCongratulations, {$username}!");
-}
+
 
