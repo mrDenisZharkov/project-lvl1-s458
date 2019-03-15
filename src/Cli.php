e4fb28b16f14394ca78f6ec4956f1de2dd034ef5
Zharkov Denis <zdu.ing@gmail.com>
2019-03-15 20:33:34 +0600

    refactor menu

--- src/Cli.php
+++ src/Cli.php
@@ -6,23 +6,35 @@ use function BrainGames\Games\Even\run as runEven;
 use function BrainGames\Games\Calc\run as runCalc;
 use function BrainGames\Games\GCD\run as runGCD;
 use function BrainGames\Games\Progression\run as runProgression;
+use function BrainGames\Games\Prime\run as runPrime;
 function runMenu()
 {
-    line("Welcome to the Brain Games!");
-    line("Menu:");
-    line("1 - Brain Even Game");
-    line("2 - Brain Calc Game");
-    line("3 - Brain GSV Game");
-    line("4 - Brain GSV Game");
-    line("any other - EXIT");
-    $userAnswer = prompt("\nEnter number of game to run it: ");
-    if ($userAnswer == 1) {
-        runEven();
-    } elseif ($userAnswer == 2) {
-        runCalc();
-    } elseif ($userAnswer == 3) {
-        runGCD();
-    } elseif ($userAnswer == 4) {
-        runProgression();
+    $games = [
+            ['title' => "Brain Even Game", 'call' => function () {
+                runEven();
+            }],
+            ['title' => "Brain Calc Game", 'call' => function () {
+                runCalc();
+            }],
+            ['title' => "Brain GSV Game", 'call' => function () {
+                runGCD();
+            }],
+            ['title' => "Brain Progression Game", 'call' => function () {
+                runProgression();
+            }],
+            ['title' => "Brain Prime Game", 'call' => function () {
+                runPrime();
+            }]
+        ];
+    line("\n\t*Welcome to the Brain Games!*");
+    line("\nMenu:");
+    foreach ($games as $index => $game) {
+        $currentIndex = $index + 1;
+        line("{$currentIndex} - {$games[$index]['title']}");
     }
+    line("any other - EXIT");
+    $selection = prompt("\nEnter number of game to run it");
+    $bye = "\nHave a good day!\n\n";
+    $commandStart = ($selection > 0 && $selection <= count($games)) ? $games[$userCommand - 1]['call']() : exit($bye);
+    return;
 }
