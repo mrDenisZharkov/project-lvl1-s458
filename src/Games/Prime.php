896a03bd3f9f23c7c177d5de028c96c0606e141e
Zharkov Denis <zdu.ing@gmail.com>
2019-03-15 22:03:16 +0600

    remark prime game

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -11,7 +11,7 @@ function isPrime($a)
     if ($a < 2) {
         return false;
     }
-    for ($i = $baseConditionPrime; $i <= $a / 2; $i++) {
+    for ($i = 2; $i <= $a / 2; $i++) {
         if ($a % $i === 0) {
             return false;
         }
